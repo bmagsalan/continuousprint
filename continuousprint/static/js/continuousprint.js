4f20053031b39a01740a6e7398c84986331c176c
DESKTOP-1Q28S2O\Brian <bmagsalan@yahoo.ca>
2020-05-21 11:04:02 -0400

    Do 2 loops to get total completed number to add.

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -34,15 +34,24 @@ $(function() {
 
 				if (r.queue.length > 0) {
 						var completedNum = 0;
+						var completedTotalNum = 0;
+						for(var i = 0; i < r.queue.length; i++) {
+							var file = r.queue[i];
+							if (file["time"] != undefined) {
+								completedTotalNum++;
+							}
+						}
 						for(var i = 0; i < r.queue.length; i++) {
 							var file = r.queue[i];
 							var row;
+							
+
 
 							if (file["time"] == undefined) {
 								var other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp; <i style='cursor: pointer' class='fa fa-chevron-up' data-index='"+i+"'></i>&nbsp;";
 								if (i == 0) other = "";
 								if (i == 1) other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp;";
-								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>"+ (i + completedNum) + ". "+file.name+"<div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
+								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>"+ (i + completedTotalNum) + ". "+file.name+"<div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
 								row.find(".fa-minus").click(function() {
 									self.removeFromQueue($(this).data("index"));
 								});
